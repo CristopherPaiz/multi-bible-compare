fedd6ad0953a2fe0163747cd111ee72b3cb2742e
CristopherPaiz <cpaizl1@miumg.edu.gt>
2024-02-18 08:52:04 -0600

    Remove unnecessary link to "/compare" in Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -23,12 +23,7 @@ const Navbar = () => {
           <div className="flex items-center">
             <ul className="flex flex-row font-medium mt-0 space-x-8 rtl:space-x-reverse text-sm">
               <li>
-                <Link to="/" className="text-gray-900 dark:text-white hover:underline" aria-current="page">
-                  {t("Inicio")}
-                </Link>
-              </li>
-              <li>
-                <Link to="/compare" className="text-gray-900 dark:text-white hover:underline">
+                <Link to="/" className="text-gray-900 dark:text-white hover:underline">
                   {t("Comparar")}
                 </Link>
               </li>
