b82f9dc6bfabf83f300cba1f115f886301f2e5ae
CristopherPaiz <cpaizl1@miumg.edu.gt>
2024-02-21 15:41:50 -0600

    Refactor imports and add navigation logic in Home component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import Navbar from "./components/Navbar";
+import { useContext } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
 import About from "./pages/About";
 import Compare from "./pages/Compare";
 import NotFound from "./pages/NotFound";
 import Settings from "./pages/Settings";
-import { useContext } from "react";
 import ThemeContext from "./context/ThemeContext";
 import Home from "./pages/Home";
 
@@ -17,6 +17,7 @@ const App = () => {
       theme === "light" ? "f5c461" : "000"
     }' fill-opacity='0.1'%3E%3Cpath fill-rule='evenodd' d='M11 0l5 20H6l5-20zm42 31a3 3 0 1 0 0-6 3 3 0 0 0 0 6zM0 72h40v4H0v-4zm0-8h31v4H0v-4zm20-16h20v4H20v-4zM0 56h40v4H0v-4zm63-25a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm10 0a3 3 0 1 0 0-6 3 3 0 0 0 0 6zM53 41a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm10 0a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm10 0a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm-30 0a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm-28-8a5 5 0 0 0-10 0h10zm10 0a5 5 0 0 1-10 0h10zM56 5a5 5 0 0 0-10 0h10zm10 0a5 5 0 0 1-10 0h10zm-3 46a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm10 0a3 3 0 1 0 0-6 3 3 0 0 0 0 6zM21 0l5 20H16l5-20zm43 64v-4h-4v4h-4v4h4v4h4v-4h4v-4h-4zM36 13h4v4h-4v-4zm4 4h4v4h-4v-4zm-4 4h4v4h-4v-4zm8-8h4v4h-4v-4z'/%3E%3C/g%3E%3C/svg%3E")`,
   };
+
   return (
     <>
       <div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,17 @@
+import { useContext, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import DataContext from "../context/DataContext";
+
 const Home = () => {
+  const { paginaInicio } = useContext(DataContext);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (paginaInicio === "/compare") {
+      navigate("/compare");
+    }
+  }, [paginaInicio, navigate]);
+
   return <div>Home</div>;
 };
 
