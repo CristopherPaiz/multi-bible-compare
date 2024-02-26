ca06ff072f67ee445f2b5dc4df53c93a2ed03af9
CristopherPaiz <cpaizl1@miumg.edu.gt>
2024-02-26 14:57:20 -0600

    Add react plugin to Vite config Fix PWA & React Dependencies

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,4 @@
 // import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react-swc'
 
 // // https://vitejs.dev/config/
 // export default defineConfig({
@@ -8,7 +7,8 @@
 
 import { defineConfig } from "vite";
 import { VitePWA } from "vite-plugin-pwa";
+import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
-  plugins: [VitePWA()],
+  plugins: [VitePWA(), react()],
 });
