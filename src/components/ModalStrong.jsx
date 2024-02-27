849d2a48db1229d593165b023912a18d55e07875
CristopherPaiz <cpaizl1@miumg.edu.gt>
2024-02-27 11:38:31 -0600

    Refactor modal and tabs styling

--- src/components/ModalStrong.jsx
+++ src/components/ModalStrong.jsx
@@ -6,23 +6,19 @@ const ModalStrong = ({ isOpen, onClose }) => {
 
   return (
     <div className="z-[9999] fixed inset-0 overflow-y-auto flex justify-center items-center">
-      <div className="absolute z-40 inset-0 bg-black/50"></div>
-      <div className="absolute z-50 flex justify-center items-center inset-0">
-        <div className="relative bg-white w-[80%] min-w-[200px] sm:min-w-[300px] sm:w-[500px] h-3/4 sm:h-[85%] p-8 rounded-lg shadow-lg">
+      <div className="absolute z-40 inset-0 bg-black/60"></div>
+      <div className="absolute z-50 flex justify-center items-center inset-0 ">
+        <div className="relative bg-white w-[90%] min-w-[200px] sm:min-w-[300px] sm:w-[500px] h-[85%] sm:h-[85%] p-2 rounded-lg shadow-lg dark:bg-gray-800 dark:text-white overflow-y-scroll no-scrollbar">
           <div className="absolute top-0 right-0">
             <button
               onClick={onClose}
-              className="bg-gray-200 text-gray-600 rounded-full w-8 h-8 mt-3 mr-3 flex items-center justify-center hover:bg-gray-300 focus:outline-none focus:ring focus:ring-gray-400"
+              className="bg-gray-200 text-gray-600 rounded-full w-8 h-8 mt-4 mr-4 flex items-center justify-center hover:bg-gray-300 focus:outline-none focus:ring focus:ring-gray-400 dark:bg-black dark:text-gray-400"
             >
               &times;
             </button>
           </div>
-          <div>
-            <h2 className="text-xl font-semibold mb-4">
-              Diccionario Strong
-              <p />
-              Griego - Hebreo
-            </h2>
+          <div className="min-w-full w-full pl-4 pr-3">
+            <h2 className="text-xl mt-3 font-semibold mb-4">Diccionario Strong</h2>
             <Tabs />
           </div>
         </div>
--- src/components/Tabs.jsx
+++ src/components/Tabs.jsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import "../styles/Tabs.css";
 
 const Tabs = () => {
   const [activeTab, setActiveTab] = useState(0);
@@ -9,24 +8,30 @@ const Tabs = () => {
   };
 
   return (
-    <div className="tabs-container">
-      <div className="tabs">
-        {/* Pestañas */}
-        <div className={`tab ${activeTab === 0 ? "active" : ""}`} onClick={() => changeTab(0)}>
+    <>
+      <div className="flex border-b border-gray-300 dark:border-gray-600 gap-x-1">
+        <div
+          className={`text-black dark:text-white py-2 px-4 cursor-pointer border border-gray-300 dark:border-gray-600 rounded-t-lg ${
+            activeTab === 0 ? "bg-white dark:bg-neutral-600" : "bg-gray-200 dark:bg-transparent opacity-40"
+          }`}
+          onClick={() => changeTab(0)}
+        >
           Hebreo
         </div>
-        <div className={`tab ${activeTab === 1 ? "active" : ""}`} onClick={() => changeTab(1)}>
+        <div
+          className={`text-black dark:text-white py-2 px-4 cursor-pointer border border-gray-300 dark:border-gray-600 rounded-t-lg ${
+            activeTab === 1 ? "bg-white dark:bg-neutral-600" : "bg-gray-200 dark:bg-transparent opacity-40"
+          }`}
+          onClick={() => changeTab(1)}
+        >
           Griego
         </div>
       </div>
-      {/* Contenido de las pestañas */}
-      <div className="tab-content">
-        {/* Contenido para la pestaña 1 */}
+      <div className="p-5 border border-gray-300 dark:border-gray-600 border-t-0 rounded-b-md">
         {activeTab === 0 && <div>Contenido de Hebreo próximamente...</div>}
-        {/* Contenido para la pestaña 2 */}
         {activeTab === 1 && <div>Contenido de Griego próximamente...</div>}
       </div>
-    </div>
+    </>
   );
 };
 
