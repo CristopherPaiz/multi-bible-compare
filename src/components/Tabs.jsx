e04f534fa66e7fa95b490c8cc29a0cc6eedc7b90
CristopherPaiz <cpaizl1@miumg.edu.gt>
2024-03-05 09:10:44 -0600

    Commented out unused code in Tabs.jsx

--- src/components/Tabs.jsx
+++ src/components/Tabs.jsx
@@ -30,19 +30,19 @@ const Tabs = () => {
         >
           {t("Griego")}
         </div>
-        <div
+        {/* <div
           className={`text-black dark:text-white py-2 px-4 cursor-pointer border border-gray-300 dark:border-gray-600 rounded-t-lg ${
             activeTab === 2 ? "bg-yellow-200 dark:bg-purple-600" : "bg-yellow-100/30 dark:bg-purple-950 opacity-40"
           }`}
           onClick={() => changeTab(2)}
         >
           {t("Cognados")}
-        </div>
+        </div> */}
       </div>
       <div className="p-5 border border-gray-300 dark:border-gray-600 border-t-0 rounded-b-md">
         {activeTab === 0 && <TabHebrew />}
         {activeTab === 1 && <TabGreek />}
-        {activeTab === 2 && <div>{t("Cognados")}</div>}
+        {/* {activeTab === 2 && <div>{t("Cognados")}</div>} */}
       </div>
     </>
   );
