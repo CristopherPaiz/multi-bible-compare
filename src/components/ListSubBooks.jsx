f5c5c4f4092d2035095cf984b07c0f135f431ba4
CristopherPaiz <cpaizl1@miumg.edu.gt>
2024-09-18 11:24:20 -0600

    Refactor ListChapters and ListSubBooks components for improved performance and code readability

--- src/components/ListSubBooks.jsx
+++ src/components/ListSubBooks.jsx
@@ -4,16 +4,8 @@ import LanguageContext from "../context/LanguageContext";
 import LEFT_ICON from "/left.png";
 
 const ListSubBooks = () => {
-  const {
-    libros,
-    setLibroSeleccionado,
-    libroSeleccionado,
-    modalLibros,
-    setModalLibros,
-    setModalChapters,
-    modoCompacto,
-    setModoCompacto,
-  } = useContext(DataContext);
+  const { libros, setLibroSeleccionado, libroSeleccionado, modalLibros, setModalLibros, setModalChapters, modoCompacto, setModoCompacto } =
+    useContext(DataContext);
   const [selectedBook, setSelectedBook] = useState(libroSeleccionado);
   const { t } = useContext(LanguageContext);
 
@@ -44,7 +36,7 @@ const ListSubBooks = () => {
         <div className="animate-slide-in-top animate-duration-100 fixed top-0 left-0 w-full h-full bg-gray-900 bg-opacity-50 flex justify-center items-center z-[99999999]">
           <div
             ref={modalRef}
-            className="z-[99999999] bg-white p-4 rounded shadow-md w-11/12 h-[85%] sm:h-[95%] flex flex-col dark:bg-black dark:text-white sm:w-[550px]"
+            className="z-[99999999] bg-white p-4 rounded shadow-md w-11/12 h-[85%] sm:h-auto flex flex-col dark:bg-black dark:text-white sm:w-[550px]"
           >
             <button className="absolute top-5 right-7 font-bold text-white text-4xl font-mono" onClick={closeModal}>
               X
@@ -140,11 +132,8 @@ const ListSubBooks = () => {
                 </div>
               </>
             )}
-            <div className="bg-white justify-center flex pt-3 gap-3 dark:bg-black">
-              <button
-                className="p-2 bg-red-500 text-white rounded text-sm flex flex-row gap-2 px-7 py-3"
-                onClick={closeModal}
-              >
+            <div className="bg-white justify-center flex pt-3 gap-3 mt-2 dark:bg-black">
+              <button className="p-2 bg-red-500 text-white rounded text-sm flex flex-row gap-2 px-7 py-3" onClick={closeModal}>
                 <img className="h-4 mt-1 invert" src={LEFT_ICON} />
                 {t("Regresar")}
               </button>
